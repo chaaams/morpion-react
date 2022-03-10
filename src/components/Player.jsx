6615c2e3b7cb72716fc4f927026dfe1d986910cd
lies93 <hocini.lies@gmail.com>
2022-03-10 16:38:20 +0100

    componnent player done

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+class Player extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            player:"O",
+        }
+    
+        
+    }
+    
+    switchClik=()=>{
+        if(this.state.player === "X"){
+            this.setState({
+                player:"O"
+            })
+        }else{
+            this.setState({
+                player:"X"
+            })
+        }
+        
+    }
+    
+
+    
+    
+    render(){
+        return(
+        <div>
+            <button onClick={this.switchClik}>test</button>
+        </div>
+        )
+    }
+}
+export default Player;
