f88f510c93979b7cc75c2a4e51a44fc73a558c9b
chaaams <diaby.chamssoudine@gmail.com>
2022-03-14 14:42:42 +0100

    style

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Player from "./components/Player"
 import React from 'react';
 import Board from "./components/Board"
 
+
 function App() {
   return (
     <div className='App'>
-      <h1 className='title'>Jeux du Morpion</h1>
+      <div className='title-container'>
+        <span className='title'> Morpion </span>
+        <span className='second-title'> Tic Tac Toe </span>
+      </div>
       <Player>Clik</Player>
       <Board></Board>
       <Player></Player>
