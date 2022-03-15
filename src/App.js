9d814789dc4f46122bf9b8a5341161ea9e6f0ed6
chaaams <diaby.chamssoudine@gmail.com>
2022-03-15 12:10:18 +0100

    features-rules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Player from "./components/Player"
 import React from 'react';
+import Cart from './components/Cart';
 // import Board from "./components/Board"
 // import SquareTest from "./components/SquareTest"
 
@@ -9,10 +10,22 @@ function App() {
   return (
     <div className='App'>
       <div className='title-container'>
-        <span className='title'> Morpion </span>
-        <span className='second-title'> Tic Tac Toe </span>
-      </div>
+        {/* <span className='title'> Morpion </span>
+        <span className='second-title'> Tic Tac Toe </span> */}
+                            <div className="wrapper">
+                                <ul className="dynamique-texte">
+                                    <li>
+                                        <span>Morpion</span>
+                                    </li>
+                                    <li>
+                                        <span>Tic Tac Toe</span>
+                                    </li>
+                                </ul>
+                            </div>
+                            
       <Player>Clik</Player>
+      </div>
+      <Cart/>
       {/* <Board></Board> */}
     </div>
   );
