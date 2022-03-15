02dc9d84765701a0a6722942d9bf6119a219db90
lies93 <hocini.lies@gmail.com>
2022-03-15 17:33:32 +0100

    function winner good

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends React.Component {
         player:"O",
         counter:0 ,
         grid :["","","","","","","","",""],
-        isWin : false
+        isWin : false,
+        winner: null
     }
 
     
@@ -27,47 +28,56 @@ switchClik=(e)=>{
       console.log(grid[id]);
       if (grid[id]=== ""){
           grid[id]= this.state.player
-          let count = this.state.counter
+          let player = null
           if(this.state.player === "X"){
-              this.setState({
-                  player:"O",
+            console.log(this.state.player)  
+            player="O"
+            this.setState({
                   grid:grid,
 
               })
           }else{
-              this.setState({
-                  player:"X",
+            console.log(this.state.player)
+            player= "X"  
+            this.setState({
                   grid : grid ,
       
               })
           }
           if(grid[0]===grid[1]&&grid[2]===grid[1] && grid[0] !== ""){
-              this.setState({isWin : true })
-               return alert('Le vainqueur est '+ this.state.player);
+            this.setState({isWin : true })
+
+            //    return alert('Le vainqueur est '+ this.state.player);
           }else if (grid[3]===grid[4]&&grid[5]===grid[3]&& grid[3] !==""){
               this.setState({isWin : true })
-              return alert("Le vainqueur est",+ this.state.player)
+            //   return alert("Le vainqueur est",+ this.state.player)
 
           }else if (grid[6]===grid[7]&&grid[8]===grid[6]&& grid[6]!==""){
               this.setState({isWin : true })
-              return alert('Le vainqueur est '+ this.state.player)
+            //   return alert('Le vainqueur est '+ this.state.player)
 
           }else if (grid[0]===grid[3]&&grid[6]===grid[3]&&grid[3]!==""){
               this.setState({isWin : true })
-              return alert('Le vainqueur est '+ this.state.player)
+            //   return alert('Le vainqueur est '+ this.state.player)
           }else if (grid[1]===grid[4]&& grid[7]===grid[4]&&grid[4]!==""){
               this.setState({isWin : true })
-              return alert('Le vainqueur est '+ this.state.player)
+            //   return alert('Le vainqueur est '+ this.state.player)
           }else if (grid[2]===grid[5]&&grid[8]===grid[5]&&grid[5] !==""){
               this.setState({isWin : true })
-              return alert ('Le vainqueur est '+ this.state.player)
+            //   return alert ('Le vainqueur est '+ this.state.player)
           }else if (grid[0]===grid[4]&&grid[8]===grid[4]&&grid[4]!==""){
               this.setState({isWin : true })
-              return alert('Le vainqueur est '+ this.state.player)
+            //   return alert('Le vainqueur est '+ this.state.player)
           }else if (grid[6]===grid[4]&&grid[2]===grid[4]&& grid[4]!==""){
               this.setState({isWin : true })
-              return alert('Le vainqueur est '+ this.state.player)
+            //   return alert('Le vainqueur est '+ this.state.player)
           }
+          let winner = this.state.player
+          this.setState({
+              player:player,
+              winner:winner,
+
+          })
 
  }
 
@@ -110,7 +120,7 @@ render = () =>{
         {/* <span className='title'> Morpion </span>
         <span className='second-title'> Tic Tac Toe </span> */}
       <Player grid={this.state.grid} reset={this.reset} switchClik={this.switchClik}></Player>
-      <Cart reset={this.reset}/>
+      <Cart reset={this.reset} isWin={this.state.isWin} playerWin={this.state.winner}/>
       </div>
       {/* <Board></Board> */}
     </div>
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import Player from "./Player";
 
+
 function Cart(props){
+
+
+     function renderWin(){
+        if(props.isWin===true){
+            return(
+    
+                <p>{props.playerWin} gagne</p>
+            )
+        }
+    }
+
+
     return(
         <div className='cart'>
             <h3>Règles</h3>
             <p>
                 Le premier joueur a aligner 3 symboles identiques gagne la partie. Attention, le joueur qui débute est toujours avantagé pour gagner. Pensez donc à alterner !
             </p>
+
+             {renderWin()}
+
             <button className='btn-grad' onClick={props.reset}>Mise a zéro du tableau</button>
             <button className='btn-grad' onClick={props.reset}>Mise a zéro du score</button>
             
