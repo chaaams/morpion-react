d52a9f01f69c70b32b9ab327471e1c818010c25b
lies93 <hocini.lies@gmail.com>
2022-03-15 12:01:11 +0100

    Function reset done

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -8,33 +8,82 @@ class Player extends React.Component{
             player:"O",
             counter:0 ,
             grid :["","","","","","","","",""],
+            isWin : false
         }
     
         
     }
 
     switchClik=(e)=>{
-        console.log(e.target.id);
-       let grid = [...this.state.grid]
-       let id = e.target.id;
-       console.log(grid[id]);
-       grid[id]= this.state.player
-        let count = this.state.counter
-        if(this.state.player === "X"){
-            this.setState({
-                player:"O",
-                grid:grid,
-            })
-        }else{
-            this.setState({
-                player:"X",
-                grid : grid ,
-            })
+        if (this.state.isWin ===false){
+            
+            console.log(e.target.id);
+            let grid = [...this.state.grid]
+            let id = e.target.id;
+            console.log(grid[id]);
+            if (grid[id]=== ""){
+                grid[id]= this.state.player
+                let count = this.state.counter
+                if(this.state.player === "X"){
+                    this.setState({
+                        player:"O",
+                        grid:grid,
+
+                    })
+                }else{
+                    this.setState({
+                        player:"X",
+                        grid : grid ,
+            
+                    })
+                }
+                if(grid[0]===grid[1]&&grid[2]===grid[1] && grid[0] !== ""){
+                    this.setState({isWin : true })
+                     return alert('win');
+                }else if (grid[3]===grid[4]&&grid[5]===grid[3]&& grid[3] !==""){
+                    this.setState({isWin : true })
+                    return alert("double win")
+
+                }else if (grid[6]===grid[7]&&grid[8]===grid[6]&& grid[6]!==""){
+                    this.setState({isWin : true })
+                    return alert("triple win")
+
+                }else if (grid[0]===grid[3]&&grid[6]===grid[3]&&grid[3]!==""){
+                    this.setState({isWin : true })
+                    return alert("ligne 1 vertical")
+                }else if (grid[1]===grid[4]&& grid[7]===grid[4]&&grid[4]!==""){
+                    this.setState({isWin : true })
+                    return alert("ligne 2 vertical")
+                }else if (grid[2]===grid[5]&&grid[8]===grid[5]&&grid[5] !==""){
+                    this.setState({isWin : true })
+                    return alert ("ligne 3 vertical")
+                }else if (grid[0]===grid[4]&&grid[8]===grid[4]&&grid[4]!==""){
+                    this.setState({isWin : true })
+                    return alert("diagonal one")
+                }else if (grid[6]===grid[4]&&grid[2]===grid[4]&& grid[4]!==""){
+                    this.setState({isWin : true })
+                    return alert("diagonal two")
+                }
+
+       }
+
         }
         // console.log(this.state.counter)
 
     }
-    
+    reset=()=>{
+        let grid = [...this.state.grid]
+        for ( let i=0 ; i < grid.length ; i++ ){
+            console.log(grid[i])
+            grid[i] = ""
+        
+        }
+            this.setState({
+                grid : grid,
+                isWin : false
+            })
+        
+    }
 
     
     
@@ -52,8 +101,7 @@ class Player extends React.Component{
                     <div className="case" id="7">{this.state.grid[7]}</div>
                     <div className="case" id="8">{this.state.grid[8]}</div>
                </div>
-            <button className='reset'>Reset</button>
-            <button onClick={this.switchClik}>Play</button>
+            <button className='reset' onClick={this.reset}>Reset</button>
         </div>
         )
     }
